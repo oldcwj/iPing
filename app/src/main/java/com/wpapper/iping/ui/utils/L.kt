9aba1fc93538493e07c0cf3859945a20316c836a
cuiwenjun <cuiwenjun@ivvi.com>
2017-10-09 22:11:00 +0800

    add doc

---
+++ app/src/main/java/com/wpapper/iping/ui/utils/L.kt
@@ -0,0 +1,38 @@
+package com.wpapper.iping.ui.utils
+
+import android.util.Log
+
+import iping.wpapper.com.iping.BuildConfig
+
+
+object L {
+
+    private val TAG_FORMAT = "%s-%s(%d)"
+
+    internal var DEBUG = BuildConfig.DEBUG
+
+    fun i(message: String?) {
+        if (DEBUG) {
+            Log.i(tag(), message ?: " NO MESSAGE ")
+        }
+    }
+
+    fun e(e: Exception) {
+        if (DEBUG) {
+            Log.e(tag(), if (e.message == null) " NO MESSAGE " else e.message, e)
+        }
+    }
+
+    fun e(message: String?) {
+        if (DEBUG) {
+            Log.e(tag(), message ?: " NO MESSAGE ")
+        }
+    }
+
+    private fun tag(): String {
+        val throwable = Throwable()
+        val target = throwable.stackTrace[2] ?: return "UNKOWN"
+        return String.format(TAG_FORMAT, target.className.substring(target.className.lastIndexOf('.') + 1), target.methodName, target.lineNumber)
+    }
+
+}
---
+++ app/src/main/java/com/wpapper/iping/ui/utils/widget/UnswipableViewPager.kt
@@ -0,0 +1,22 @@
+package com.wpapper.iping.ui.utils.widget
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.MotionEvent
+
+
+class UnswipableViewPager : ViewPager {
+
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        return false
+    }
+}
